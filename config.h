080d27e32982c235822a49f62d2a0ffe15012b8a
Umang Bhalla <umg.bhalla88@gmail.com>
2021-06-21 17:35:09 +0530

    changed logo

--- config.h
+++ config.h
@@ -1,40 +1,33 @@
 #include "logos/arch.h"
-#define COLOR "\e[1;36m"
+#define COLOR "\e[1;35m"
 
 #define CONFIG \
 { \
    /* name            function                 cached */\
     { "",             get_title,               false }, \
-    { "",             get_bar,                 false }, \
-    { "OS: ",         get_os,                  true  }, \
-    { "Host: ",       get_host,                true  }, \
-    { "Kernel: ",     get_kernel,              true  }, \
-    { "Uptime: ",     get_uptime,              false }, \
-    { "Battery: ",    get_battery_percentage,  false }, \
-    SPACER \
-    { "Packages: ",   get_packages_pacman,   false }, \
-    { "Shell: ",      get_shell,             false }, \
-    { "Resolution: ", get_resolution,        false }, \
-    { "Terminal: ",   get_terminal,          false }, \
-    SPACER \
-    { "CPU: ",        get_cpu,                 true  }, \
-    { "GPU: ",        get_gpu1,                true  }, \
-    { "Memory: ",     get_memory,              false }, \
-    SPACER \
-    { "",             get_colors1,             false }, \
+    { "",             get_bar,                 true }, \
+    { "  os ~ ",         get_os,                  true  }, \
+    { "host ~ ",       get_host,                true  }, \
+    { "  up ~ ",     get_uptime,              true }, \
+    { " bat ~ ",    get_battery_percentage,  true }, \
+    { "  sh ~ ",      get_shell,             true }, \
+    { " res ~ ", get_resolution,        true }, \
+    { " cpu ~ ",        get_cpu,                 true  }, \
+    { " gpu ~ ",        get_gpu1,                true  }, \
+    { " mem ~ ",     get_memory,              true }, \
+    { "kern ~ ",     get_kernel,              true  }, \
+    { "pkgs ~ ",   get_packages_pacman,   true }, \
+    { "term ~ ",   get_terminal,          true }, \
+	SPACER \
+	{ "",             get_colors1,             false }, \
     { "",             get_colors2,             false }, \
 }
 
 #define CPU_CONFIG \
 { \
-   REMOVE("(R)"), \
-   REMOVE("(TM)"), \
-   REMOVE("Dual-Core"), \
-   REMOVE("Quad-Core"), \
-   REMOVE("Six-Core"), \
-   REMOVE("Eight-Core"), \
-   REMOVE("Core"), \
-   REMOVE("CPU"), \
+   REMOVE("with"), \
+   REMOVE("Radeon"), \
+   REMOVE("Graphics"), \
 }
 
 #define GPU_CONFIG \
