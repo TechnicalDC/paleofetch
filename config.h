9278c85f1509ab904ee67c4c77272e8c3e5bbbcf
Umang Bhalla <umg.bhalla88@gmail.com>
2021-06-24 23:33:05 +0530

    updated cache states and ascii logo

--- config.h
+++ config.h
@@ -1,23 +1,22 @@
-#include "logos/arch.h"
+#include "logos/term1.h"
 #define COLOR "\e[1;35m"
 
 #define CONFIG \
 { \
    /* name            function                 cached */\
     { "",             get_title,               false }, \
-    { "",             get_bar,                 true }, \
     { "  os ~ ",         get_os,                  true  }, \
-    { "host ~ ",       get_host,                true  }, \
-    { "  up ~ ",     get_uptime,              true }, \
-    { " bat ~ ",    get_battery_percentage,  true }, \
     { "  sh ~ ",      get_shell,             true }, \
+    { "  up ~ ",     get_uptime,              false }, \
+    { " bat ~ ",    get_battery_percentage,  false }, \
     { " res ~ ", get_resolution,        true }, \
     { " cpu ~ ",        get_cpu,                 true  }, \
     { " gpu ~ ",        get_gpu1,                true  }, \
-    { " mem ~ ",     get_memory,              true }, \
+    { " mem ~ ",     get_memory,              false }, \
     { "kern ~ ",     get_kernel,              true  }, \
-    { "pkgs ~ ",   get_packages_pacman,   true }, \
-    { "term ~ ",   get_terminal,          true }, \
+    { "pkgs ~ ",   get_packages_pacman,   false }, \
+    { "term ~ ",   get_terminal,          false }, \
+    { "host ~ ",       get_host,                true  }, \
 	SPACER \
 	{ "",             get_colors1,             false }, \
     { "",             get_colors2,             false }, \
